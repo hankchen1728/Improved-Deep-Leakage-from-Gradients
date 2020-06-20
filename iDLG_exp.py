ab19b77406a82b111cb6df8870385f0aa3b33feb
hankchen1728 <hankchen1728@gmail.com>
2020-06-20 16:24:16 +0800

    update exp

--- iDLG_exp.py
+++ iDLG_exp.py
@@ -9,9 +9,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 from torchvision import datasets, transforms
-from models import ConvNet
+from models import ConvNet, config_resnet18
 from dataset import CheXpertDataset
-# from iDLG import LeNet, weights_init
 
 
 class ImageDataset(Dataset):
@@ -53,7 +52,10 @@ def lfw_dataset(lfw_path, shape_img):
 
 def config_net(net_name="", input_shape=(3, 32, 32), num_classes=10):
     assert net_name in ["CNN_L2D1", "CNN_L2D2",
-                        "CNN_L4D1", "CNN_L4D2", "CNN_L6D2"]
+                        "CNN_L4D1", "CNN_L4D2",
+                        "CNN_L4D4", "CNN_L6D2",
+                        "CNN_L7D2", "ResNet18"], "{}".format(net_name)
+
     if net_name == "CNN_L2D1":
         conv_channels = [32, 64]
     elif net_name == "CNN_L2D2":
@@ -62,8 +64,19 @@ def config_net(net_name="", input_shape=(3, 32, 32), num_classes=10):
         conv_channels = [32, 64, 128, 256]
     elif net_name == "CNN_L4D2":
         conv_channels = [64, 128, 256, 512]
+    elif net_name == "CNN_L4D4":
+        conv_channels = [128, 256, 512, 1024]
     elif net_name == "CNN_L6D2":
         conv_channels = [32, 64, 64, 128, 128, 256]
+    elif net_name == "CNN_L7D2":
+        conv_channels = [64, 64, 64, 128, 128, 256, 256]
+    elif net_name == "ResNet18":
+        net = config_resnet18(
+            input_channels=input_shape[0],
+            num_classes=num_classes)
+        return net
+        # end switch
+
     net = ConvNet(
         image_shape=input_shape,
         conv_channels=conv_channels,
@@ -85,17 +98,14 @@ def main(args):
         root_path, "results", "iDLG_%s_%s" % (dataset, net_name)
     )
 
-    if args.refined_dummy_init:
-        save_path += "_init"
-
     if args.add_clamp:
         save_path += "_clamp"
 
-    # lr = 1.0
+    # Some running setting
     initial_lr = args.lr
     num_dummy = 1
     Iteration = args.max_iter
-    num_exp = 30
+    plot_steps = args.plot_steps
     # run_methods = ["iDLG", "DLG"]
     run_methods = ["iDLG"]
 
@@ -115,7 +125,6 @@ def main(args):
     selected_indices = []
     cmap = "viridis"
     if dataset == 'MNIST':
-        cmap = "gray"
         shape_img = (28, 28)
         num_classes = 10
         channel = 1
@@ -139,18 +148,6 @@ def main(args):
         channel = 3
         # hidden = 768
         dst = datasets.CIFAR10(data_path, download=True)
-        # selected_indices = np.array([
-        #     29, 30, 35,
-        #     4,  5, 32,
-        #     6, 13, 18,
-        #     9, 17, 21,
-        #     3, 10, 20,
-        #     27, 40, 51,
-        #     0, 19, 22,
-        #     7, 11, 12,
-        #     8, 62, 69,
-        #     1,  2, 14
-        # ])
         selected_indices = np.array([
             29,  30,  35,  49,  77,  93, 115, 116, 129, 165,
             4,   5,  32,  44,  45,  46,  60,  61,  64,  65,
@@ -179,15 +176,20 @@ def main(args):
         dst = lfw_dataset(lfw_path, shape_img)
 
     elif dataset == 'CheXpert':
-        shape_img = (112, 112)
+        shape_img = (224, 224)
         num_classes = 2
-        channel = 3
+        channel = 1
         resize_t = transforms.Resize(shape_img)
         dst = CheXpertDataset(
-            csv_path='./idlg_data_entry.csv', transforms=resize_t)
+            csv_path='./idlg_data_entry.csv',
+            channels=channel,
+            transforms=resize_t)
         selected_indices = np.arange(0, 30, 3)
     else:
         exit('unknown dataset')
+
+    if channel == 1:
+        cmap = "gray"
 
     # Build ConvNet
     net = config_net(
@@ -203,6 +205,7 @@ def main(args):
         net.load_state_dict(ckpt)
 
     num_success = 0
+    num_exp = len(selected_indices)
     criterion = nn.CrossEntropyLoss().to(device)
     ''' train DLG and iDLG '''
     for idx_exp in range(num_exp):
@@ -242,7 +245,7 @@ def main(args):
                 ).to(device).requires_grad_(True)
 
             # truncated dummy image and label
-            if args.refined_dummy_init:
+            if args.add_clamp:
                 dummy_data.data = torch.clamp(dummy_data + 0.5, 0, 1)
                 dummy_label.data = torch.clamp(dummy_label + 0.5, 0, 1)
 
@@ -304,6 +307,25 @@ def main(args):
                 # pixel value clip
                 if args.add_clamp:
                     dummy_data.data = torch.clamp(dummy_data, 0, 1)
+                # print(dummy_data.data.max(), dummy_data.data.min())
+                # if args.add_clamp:
+                #     # Min-max normalization
+                #     dummy_max = dummy_data.data.max()
+                #     dummy_min = dummy_data.data.min()
+                #     dummy_diff = dummy_max - dummy_min
+                #     if dummy_diff != 0:
+                #         if dummy_max > 1 and dummy_min > 0:
+                #             dummy_data.data = \
+                #                 (dummy_data - dummy_min) / dummy_diff \
+                #                 * (1 - dummy_min) + dummy_min
+                #         elif dummy_max < 1 and dummy_min < 0:
+                #             dummy_data.data = \
+                #                 (dummy_data - dummy_min) / dummy_diff \
+                #                 * dummy_max
+                #         elif dummy_max > 1 and dummy_min < 0:
+                #             dummy_data.data = \
+                #                 (dummy_data - dummy_min) / dummy_diff
+                #         print(dummy_data.data.max(), dummy_data.data.min())
 
                 current_loss = closure().item()
                 train_iters.append(iters)
@@ -311,8 +333,7 @@ def main(args):
                 mses.append(torch.mean((dummy_data-gt_data)**2).item())
                 scheduler.step()
 
-                # if iters % int(Iteration / 30) == 0:
-                if iters % 5 == 0:
+                if iters % plot_steps == 0:
                     current_time = str(time.strftime(
                         "[%Y-%m-%d %H:%M:%S]", time.localtime()))
                     print(current_time, iters,
@@ -332,18 +353,14 @@ def main(args):
                             plt.imshow(history[i][imidx], cmap=cmap)
                             plt.title('iter=%d' % (history_iters[i]))
                             plt.axis('off')
-                        if method == 'DLG':
-                            plt.savefig(
-                                '%s/DLG_on_%05d.png' %
-                                (save_path, selected_indices[idx_exp])
-                                )
-                            plt.close()
-                        elif method == 'iDLG':
-                            plt.savefig(
-                                '%s/iDLG_on_%05d.png' %
-                                (save_path, selected_indices[idx_exp])
-                                )
-                            plt.close()
+
+                        # save plot figure
+                        plt.savefig(
+                            '%s/%s_on_%05d.png' %
+                            (save_path, method, selected_indices[idx_exp]),
+                            dpi=200, bbox_inches="tight"
+                            )
+                        plt.close()
 
                     # if current_loss < 0.000001:
                     # converge
@@ -405,8 +422,8 @@ if __name__ == '__main__':
             "--cnn_name",
             type=str,
             default="CNN_L2D1",
-            choices=["CNN_L2D1", "CNN_L2D2", "CNN_L4D1",
-                     "CNN_L4D2", "CNN_L6D2"],
+            choices=["CNN_L2D1", "CNN_L2D2", "CNN_L4D1", "CNN_L4D2",
+                     "CNN_L4D4", "CNN_L6D2", "CNN_L7D2", "ResNet18"],
             help="CNN config")
 
     parser.add_argument(
@@ -428,15 +445,17 @@ if __name__ == '__main__':
             help="maximum iterations")
 
     parser.add_argument(
+            "--plot_steps",
+            type=int,
+            default=5,
+            help="steps to plot the results")
+
+    parser.add_argument(
             "--dataset",
             type=str,
             default="MNIST",
             help="use image dataset",
             choices=["MNIST", "cifar10", "cifar100", "CheXpert"])
-
-    parser.add_argument(
-            "--refined_dummy_init",
-            action="store_true")
 
     parser.add_argument(
             "--add_clamp",
